3dee7f805a42cba7545dcdeec0ed7849edcf695a
shin donggyu <donggyu9410@gmail.com>
2019-08-28 11:15:30 +0900

    card socket server done

--- src/CardSocketServer.js
+++ src/CardSocketServer.js
@@ -18,8 +18,131 @@ io.on('connection', socket => {
         const { userEmail, cardId } = data;
         socket.userEmail = userEmail;
         socket.cardId = cardId;
+        let exist = false;
+        clients.map(client => {
+            if (client.userEmail === socket.userEmail && client.cardId === socket.cardId) {
+                exist = true;
+            } else {
+                exist = false;
+            }
+        })
+
         clients.push(socket);
         console.log('User login: ', userEmail, cardId);
+
+
+    })
+
+    socket.on('edit-card-description', (data) => {
+        // data should be card object
+        // Find targets who has same cardId with sendor but not sender. 
+
+        const targets = clients.filter(client => {
+            if (client.cardId === socket.cardId && client.userEmail !== socket.userEmail) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        targets.map(target => {
+            target.emit('edit-card-description', data)
+        })
+    })
+
+    socket.on('delete-comment', data => {
+        // data will be a comment object 
+        // Find targets who has same cardId with sendor but not sender. 
+
+        const targets = clients.filter(client => {
+            if (client.cardId === socket.cardId && client.userEmail !== socket.userEmail) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        targets.map(target => {
+            target.emit('delete-comment', data)
+        })
+    })
+
+    socket.on('add-comment', data => {
+        // data will be a comment object 
+        // Find targets who has same cardId with sendor but not sender. 
+
+        const targets = clients.filter(client => {
+            if (client.cardId === socket.cardId && client.userEmail !== socket.userEmail) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        targets.map(target => {
+            target.emit('add-comment', data)
+        })
+    })
+
+    socket.on('delete-checklist', data => {
+        // data will be a checklist object
+        // Find targets who has same cardId with sendor but not sender. 
+
+        const targets = clients.filter(client => {
+            if (client.cardId === socket.cardId && client.userEmail !== socket.userEmail) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        targets.map(target => {
+            target.emit('delete-checklist', data)
+        })
+    })
+
+    socket.on('edit-checklist', data => {
+        // data will be a checklist object
+        // Find targets who has same cardId with sendor but not sender. 
+
+        const targets = clients.filter(client => {
+            if (client.cardId === socket.cardId && client.userEmail !== socket.userEmail) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        targets.map(target => {
+            target.emit('edit-checklist', data)
+        })
+    })
+
+    socket.on('toggle-checklist', data => {
+        // data should be a checklist id
+        // Find targets who has same cardId with sendor but not sender. 
+
+        const targets = clients.filter(client => {
+            if (client.cardId === socket.cardId && client.userEmail !== socket.userEmail) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        targets.map(target => {
+            target.emit('toggle-checklist', data)
+        })
+    })
+
+    socket.on('add-new-checklist', data => {
+        // data should be checklist object
+        // Find targets who has same cardId with sendor but not sender. 
+
+        const targets = clients.filter(client => {
+            if (client.cardId === socket.cardId && client.userEmail !== socket.userEmail) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        targets.map(target => {
+            target.emit('add-new-checklist', data)
+        })
     })
 
     socket.on('leave-card-detail', () => {
